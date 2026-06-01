2a847def3b19b841df6487f7ea8cf62db375de68
aper0088 <140810069+aper0088@users.noreply.github.com>
2026-06-01 23:14:40 +1000

    Initial Commit

---
+++ vega_lite_vis.js
@@ -0,0 +1,60 @@
+// var vg_1 = "Graph.vg.json";
+// vegaEmbed("#bar_chart", vg_1).then(function(result) {
+    
+// }).catch(console.error);
+
+var vg_2 = "StopsGraph.vg.json";
+vegaEmbed("#stop", vg_2).then(function(result) {
+    
+}).catch(console.error);
+
+
+var vg_3 = "servicesPer10k.vg.json";
+vegaEmbed("#services", vg_3).then(function(result) {
+    
+}).catch(console.error);
+
+var vg_4 = "servicesVsPopulation.vg.json";
+vegaEmbed("#population", vg_4).then(function(result) {
+    
+}).catch(console.error);
+
+var vg_5 = "metroVSregional.vg.json";
+vegaEmbed("#metroVSregional", vg_5).then(function(result) {
+    
+}).catch(console.error);
+
+var vg_6 = "accessibility.vg.json";
+vegaEmbed("#accessibility", vg_6).then(function(result) {
+    
+}).catch(console.error);
+
+
+var vg_11 = "accessibility_2.vg.json";
+vegaEmbed("#accessibility_2", vg_11).then(function(result) {
+    
+}).catch(console.error);
+
+var vg_7 = "peakTimes.vg.json";
+vegaEmbed("#peakTimes", vg_7).then(function(result) {
+    
+}).catch(console.error);
+
+var vg_8 = "stopVSlga.vg.json";
+vegaEmbed("#stopVSlga", vg_8).then(function(result) {
+    
+}).catch(console.error);
+
+
+
+
+var vg_9 = "first.vg.json";
+vegaEmbed("#first", vg_9).then(function(result) {
+    
+}).catch(console.error);
+
+var vg_10 = "last.vg.json";
+vegaEmbed("#last", vg_10).then(function(result) {
+    
+}).catch(console.error);
+
